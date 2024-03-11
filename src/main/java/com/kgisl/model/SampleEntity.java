7d240b64762e296f6e41b2d74ef0eb2e864e5c2c
vigneshs173 <vickyroman404@gmail.com>
2024-03-11 08:20:32 +0530

    added new

--- src/main/java/com/kgisl/model/SampleEntity.java
+++ src/main/java/com/kgisl/model/SampleEntity.java
@@ -17,4 +17,13 @@ public class SampleEntity {
     private Long test1;
     private String test2;
     private String test3;
+    private Long test4;
+    private String test5;
+    private String test6;
+    private Long test7;
+    private String test8;
+    private String test9;
+    private Long test10;
+    private String test11;
+    private String test12;
 }
