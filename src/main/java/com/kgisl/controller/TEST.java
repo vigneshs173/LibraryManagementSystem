62a4b5f14c2291bba343e348b20d70f767a68115
vigneshs173 <vickyroman404@gmail.com>
2024-03-11 08:43:22 +0530

    added sample

---
+++ src/main/java/com/kgisl/controller/TEST.java
@@ -0,0 +1,11 @@
+package com.kgisl.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@Controller
+public class TEST {
+
+
+    //Test Controller
+}
